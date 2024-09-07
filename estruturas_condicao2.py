b7e44204480b0d9819b522d2db777cdffdfa7fd6
Athos França <108290053+xxmelkezedek@users.noreply.github.com>
2024-09-07 19:30:22 -0300

    Add files via upload

---
+++ estruturas_condicao2.py
@@ -0,0 +1,11 @@
+tempoExperiencia = 3
+
+if tempoExperiencia < 2:
+    print('Nível de conhecimento júnior.')
+
+elif tempoExperiencia > 2 and tempoExperiencia < 5:
+    print('Nível de conhecimento pleno.')
+
+else:
+    print('Nivel de conhecimento sênior.')
+
