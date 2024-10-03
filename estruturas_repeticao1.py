10a3f9f29e5ae58c6c47d990c2d171529fd1587e
Athos França <108290053+xxmelkezedek@users.noreply.github.com>
2024-10-03 04:11:59 -0300

    Update estruturas_repeticao1.py

--- estruturas_repeticao1.py
+++ estruturas_repeticao1.py
@@ -1,9 +1,9 @@
 entrada_idade = ''
 
-while entrada_idade != 0:
+while str(entrada_idade) != 0:
     try:
         entrada_idade = int(input('Digite um número qualquer ou 0 para sair: '))
-        print('Número digitado:', entrada_idade)
+        print(f'Número digitado: {entrada_idade}')
     except ValueError:
         print('Entrada inválida! Digite um número inteiro.')
 
